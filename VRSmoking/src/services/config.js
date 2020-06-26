0683d2003342ead4437e26cb0829c7b7bf7ca942
LUCAS PEREIRA DE LIRA <1183@uaa.local>
2020-06-26 10:35:00 -0300

    Removendo URL da base de teste da Alto Alegre

--- VRSmoking/src/services/config.js
+++ VRSmoking/src/services/config.js
@@ -2,8 +2,7 @@
 config = {
     URL: {
         VRSMOKING_API: 'http://192.168.0.105:3000',
-        VRSMOKING_CHAT: 'http://192.168.0.105:5000',
-        //ALT: 'http://portalteste.altoalegre.com.br'
+        VRSMOKING_CHAT: 'http://192.168.0.105:5000'
     }
 }
 
