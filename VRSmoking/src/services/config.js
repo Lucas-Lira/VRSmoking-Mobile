4b1847c01fdba191802d5abe8c45bd073248cf83
LucasLira123 <lucaspereiradelira@gmail.com>
2020-08-15 11:01:49 -0300

    atualizando

--- VRSmoking/src/services/config.js
+++ VRSmoking/src/services/config.js
@@ -1,8 +1,8 @@
 // Futuramente configurar para base de teste e produção
 config = {
     URL: {
-        VRSMOKING_API: `https://vrsmoking-api.herokuapp.com`,//'http://192.168.0.102:3000',
-        VRSMOKING_CHAT: 'http://192.168.0.102:6000', // 'https://vrsmoking-api-chat.herokuapp.com'
+        VRSMOKING_API: `https://vrsmoking-api.herokuapp.com`,
+        VRSMOKING_CHAT: 'https://vrsmoking-api-chat.herokuapp.com'
     }
 }
 
